afd3682598f81f39fc3b94196a4cca28262e42bf
Trikita73 <diachenkoandrii7@gmail.com>
2024-06-14 12:41:15 +0300

    aoi_message_bot

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
-
+from aiogram.filters import CommandStart, Command
 
 bot = Bot(token='7083250219:AAHj6bwCq-RvWU3r6nmP1irXcWCwfoiwPOU')
 dp = Dispatcher()
 
 
-@dp.message()
-async def cmd_start(message):
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
     await message.answer('Привет!')
     await message.reply('Как дела?')
+
+@dp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer('Вы нажали на кнопку помощи')
+
+@dp.message(F.text == 'Все хорошо')
+async def nice(message: Message):
+    await message.answer('Отлично!')
 
 async def main():
     await dp.start_polling(bot)
