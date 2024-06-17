1817ea906601dc3c6ad721b4601af12ec70625d5
Trikita73 <diachenkoandrii7@gmail.com>
2024-06-17 13:57:01 +0300

    bot_category

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
 import app.keyboards as kb
@@ -17,6 +17,12 @@ async def cmd_start(message: Message):
 async def cmd_help(message: Message):
     await message.answer('Вы нажали на кнопку помощи')
 
-@router.message(F.text == 'Все хорошо')
-async def nice(message: Message):
-    await message.answer('Отлично!')
+@router.message(F.text == 'Каталог')
+async def catalog(message: Message):
+    await message.answer('Выберите категрию товара', reply_markup=kb.catalog)
+
+
+@router.callback_query(F.data == 't-shirt')
+async def t_shirt(callback: CallbackQuery):
+        await callback.answer('Вы выбрали категорю', show_alert=True)
+        await callback.message.answer('Вы выбрали категорию футболок')
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,8 +1,18 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                                InlineKeyboardMarkup, InlineKeyboardButton)
 
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
                                      [KeyboardButton(text='Корзина')],
                                      [KeyboardButton(text='Контакты'), KeyboardButton(text='О нас')]
                                     ],
                             resize_keyboard=True,
-                            imput_field_placeholder='Выберите пункт меню...')
+                            input_field_placeholder='Choise menu item...')
+
+
+# inline keyboard 
+
+
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='T-shirt', callback_data='t-shirt')],
+        [InlineKeyboardButton(text='Shoes', callback_data='shoes')],
+        [InlineKeyboardButton(text='Caps', callback_data='cap')]])
