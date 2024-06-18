16e629ba97b822256b525d740f1290fb23e895f2
Trikita73 <diachenkoandrii7@gmail.com>
2024-06-18 15:47:53 +0300

    database_bot

--- app/database/models.py
+++ app/database/models.py
@@ -18,7 +18,7 @@ class User(Base):
 class Category(Base):
     __tablename__ = 'categories'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True) 
     name: Mapped[str] = mapped_column(String(25))
 
 class Item(Base):
