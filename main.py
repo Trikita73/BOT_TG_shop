cc7c051ed1b15281c6120ae5bff1f16c0082dc0d
Trikita73 <diachenkoandrii7@gmail.com>
2024-06-18 15:30:21 +0300

    database

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 
 from app.handlers import router
+from app.database.models import async_main
 
 async def main():
-    bot = Bot(token='') 
+    await async_main()
+    bot = Bot(token='7083250219:AAHj6bwCq-RvWU3r6nmP1irXcWCwfoiwPOU') 
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
