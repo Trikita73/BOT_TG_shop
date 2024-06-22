61fe05db0c964255f1f6eed005b577eb460401ec
Trikita73 <diachenkoandrii7@gmail.com>
2024-06-22 14:00:51 +0300

    database

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 
 from app.handlers import router
-from app.database.models import async_main
+from app.database.models import async_main 
 
 async def main():
     await async_main()
